b01725759fee669bbd129f61884cb004238ed50e
stant <stantondobson@gmail.com>
2020-05-10 16:54:39 -0400

    initial commit ZTM template

---
+++ gatsby-blog/gatsby-blog/src/pages/page-3.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { PageProps, Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const ThirdPage = (props: PageProps) => (
+  <Layout>
+    <h1>Hi from the third page</h1>
+    
+  </Layout>
+)
+
+export default ThirdPage
+
